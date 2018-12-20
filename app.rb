de04990ede1f6cc3d9c86d3a26333410acc3d013
Connie Hyman <constance.hyman@mchsi.com>
2018-12-20 09:32:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,10 +20,10 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
-    session["foo"] == ["hello"]
-    @session = session
+    session["foo"] = ["hello"]
+  
 
-    if session[:foo] == 'hello'
+    if session[foo] == 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
